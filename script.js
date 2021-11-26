8a83ef72b8372d4b6a36cdf07c77a06cef6e6bf8
DJ Asuncion <22920210+djasuncion@users.noreply.github.com>
2021-11-26 14:56:53 +0800

    pip working

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log('Hi!');
+const videoElement = document.getElementById('video');
+
+// Prompt user to select a media stream, pass to video element, then play
+
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (e) {
+    // Catch errors
+    console.log('woops! error here:', e);
+  }
+}
+
+button.addEventListener('click', async () => {
+  // Disable button
+  button.disabled = true;
+  // Start PIP
+  await videoElement.requestPictureInPicture();
+  // Reset Button
+  button.disabled = false;
+});
+
+// On Load
+selectMediaStream();
